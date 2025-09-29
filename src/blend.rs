b2e6fb58597f56f78604bf28a535da05ec5284bb
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-09-29 16:34:19 +0100

    Deprecate `Mix::Clip`

--- src/blend.rs
+++ src/blend.rs
@@ -84,8 +84,13 @@ pub enum Mix {
     ///
     /// ![](https://www.w3.org/TR/compositing-1/examples/luminosity.png)
     Luminosity = 15,
-    /// `Clip` is the same as `Normal`, but the latter always creates an isolated blend group and the
-    /// former can optimize that out.
+    /// Clip was similar to [`Normal`], but was optimised for clipping (by avoiding
+    /// blending in areas where there was no clip path).
+    ///
+    /// This optimisation is however unrelated to mixing, and so is no longer indicated with this enum.
+    ///
+    /// If you were using this with Vello, you should use `push_clip_layer` function instead.
+    #[deprecated(note = "Use `push_clip_layer` instead.", since = "0.5.0")]
     Clip = 128,
     // NOTICE: If a new value is added, be sure to update the bytemuck CheckedBitPattern impl.
 }
