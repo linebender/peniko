250b3d9ea323cecd1f6dba640580a977ccfb4543
sagudev <16504129+sagudev@users.noreply.github.com>
2025-10-06 07:32:02 +0200

    Derive `Eq` and `Hash` on `InterpolationAlphaSpace`
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- src/gradient.rs
+++ src/gradient.rs
@@ -232,7 +232,7 @@ impl SweepGradientPosition {
 
 /// Defines how color channels should be handled when interpolating
 /// between transparent colors.
-#[derive(Clone, Copy, Default, Debug, PartialEq)]
+#[derive(Clone, Copy, Default, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum InterpolationAlphaSpace {
     /// Colors are interpolated with their color channels premultiplied by the alpha
